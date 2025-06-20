06e0cf2eb9ccb0ef18b19cf5f69199bd0e602c22
Kara Woo <kara.woo@posit.co>
2025-06-20 14:36:02 -0700

    vecorize gh_encode

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -4,13 +4,18 @@ use geohash::{encode, Coord};
 /// Print coordinate
 /// @export
 #[extendr]
-fn gh_encode(x: f64, y: f64, length: usize) -> String {
+fn gh_encode(x: Vec<f64>, y: Vec<f64>, length: usize) -> Vec<String> {
     if length < 1 || length > 12 {
         throw_r_error("`length` must be in [1, 12]")
     }
-    let coord = Coord { x, y };
-    let encoded = encode(coord, length);
-    encoded.unwrap()
+    x.into_iter()
+        .zip(y.into_iter())
+        .map(|(xi, yi)| {
+            let coord = Coord { x: xi, y: yi };
+            let encoded = encode(coord, length);
+            encoded.unwrap()
+        })
+        .collect::<Vec<String>>()
 }
 
 // Macro to generate exports.
